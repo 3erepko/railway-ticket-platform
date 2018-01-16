b95871d7b049e329764d1c0c034d12ba138d7282
Honey Badger <pfnet77@gmail.com>
2018-01-16 19:33:53 +0100

    Create initial configuration
    Includes gradle, spring boot, react and JUnit5 config

---
+++ backend/src/main/java/app/railway/ticket/platform/RailwayTicketPlatformApplication.java
@@ -0,0 +1,12 @@
+package app.railway.ticket.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayTicketPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayTicketPlatformApplication.class, args);
+	}
+}
---
+++ backend/src/main/java/app/railway/ticket/platform/ServletInitializer.java
@@ -0,0 +1,13 @@
+package app.railway.ticket.platform;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RailwayTicketPlatformApplication.class);
+	}
+
+}
