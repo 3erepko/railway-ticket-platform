1f912af984465ead963faca84c5541d898e3ffea
Jonasz Czerepko <rajek7@gmail.com>
2018-01-27 19:24:18 +0100

    Add SCSS configuration for Webpack+React. Add ESLint configuration. Remove unused modules. Refactor Webpack config file to comply with the new recommendations.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {AppContainer} from 'react-hot-loader';
-import MainContainer from './MainContainer';
+import React from "react";
+import ReactDOM from "react-dom";
+import { AppContainer } from "react-hot-loader";
+
+import MainContainer from "./MainContainer";
 
 const render = Component => {
-    ReactDOM.render(
-        <AppContainer>
-            <Component/>
-        </AppContainer>,
-        document.getElementById('root'),
-    )
-}
+  ReactDOM.render(
+      <AppContainer>
+        <Component/>
+      </AppContainer>,
+      document.getElementById("root")
+  );
+};
 
 render(MainContainer);
 
 if (module.hot) {
-    module.hot.accept('./MainContainer', () => {
-        render(MainContainer)
-    })
+  module.hot.accept("./MainContainer", () => {
+    render(MainContainer);
+  });
 }
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,44 +1,55 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry : ['react-hot-loader/patch', './src/index.js'],
-    output : {
-        path : path.resolve(__dirname, 'public'),
-        publicPath : '/',
-        filename : 'bundle.js'
-    },
-    module : {
-        loaders : [
-            {
-                test : /(\.js|\.jsx)$/,
-                loader : 'babel-loader',
-                exclude : /node_modules/,
-                query : {
-                    presets : ['es2017', 'react']
-                }
-            },
-            {
-                test : /(\.scss|\.css)$/,
-                use : [
-                    {loader : "style-loader"},
-                    {
-                        loader : "css-loader",
-                        options : {
-                            modules : true,
-                            sourceMap : true,
-                            importLoaders : 1,
-                            localIdentName : "[name]--[local]--[hash:base64:8]"
-                        }
-                    },
-                    {loader : "postcss-loader"}
-                ]
+  entry: ["react-hot-loader/patch", "./src/index.js"],
+  output: {
+    path: path.resolve(__dirname, "public"),
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.js|\.jsx)$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        query: {
+          presets: ["es2017", "react"]
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader"
+          }, {
+            loader: "css-loader"
+          }, {
+            loader: "sass-loader"
+          }]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              sourceMap: true,
+              importLoaders: 1,
+              localIdentName: "[name]--[local]--[hash:base64:8]"
             }
+          },
+          { loader: "postcss-loader" }
         ]
-    },
-    devServer : {
-        contentBase : path.join(__dirname, 'public'),
-        host : 'localhost',
-        port : 8081,
-        hot : true
-    }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    host: "localhost",
+    port: 8081,
+    hot: true
+  }
 };
